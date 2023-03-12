120c79b0af3b48b264ebc5ddd4cc943ce2f65d9c
Jacqueline McCleary <jmac.ftw@gmail.com>
2023-03-11 20:46:44 -0500

    add grid, change area

--- mini_medsmaker/make_plots.py
+++ mini_medsmaker/make_plots.py
@@ -27,8 +27,8 @@ def set_rc_params():
     '''
     plt.rcParams.update({'figure.facecolor':'w'})
     plt.rcParams.update({'axes.linewidth': 1.0})
-    plt.rcParams.update({'xtick.labelsize': 14})
-    plt.rcParams.update({'ytick.labelsize': 14})
+    plt.rcParams.update({'xtick.labelsize': 16})
+    plt.rcParams.update({'ytick.labelsize': 16})
     plt.rcParams.update({'xtick.major.size': 8})
     plt.rcParams.update({'xtick.major.width': 1.0})
     plt.rcParams.update({'xtick.minor.visible': True})
@@ -42,7 +42,7 @@ def set_rc_params():
     plt.rcParams.update({'ytick.minor.size':6})
     plt.rcParams.update({'ytick.direction':'out'})
     plt.rcParams.update({'font.family': 'serif'})
-    plt.rcParams.update({'font.size': '14'})
+    plt.rcParams.update({'font.size': '16'})
     return
 
 
@@ -52,7 +52,7 @@ def main(args):
     set_rc_params()
     
    # Create the joined plot and axis objects
-    fig1, ax_joined = plt.subplots(figsize=(8,4), tight_layout=True)
+    fig1, ax_joined = plt.subplots(figsize=(8,5), tight_layout=True)
 
     # Call the plot_joined_data function for each CSV file to plot
     for i in range(1, 7):
@@ -61,7 +61,7 @@ def main(args):
             joined_xlabel=args['joined_xlabel'], joined_ylabel=args['joined_ylabel'], joined_title=args['joined_title'])
 
     # Adjust the plot margins to ensure all axes labels are visible
-    plt.subplots_adjust(left=0.1, right=0.95, bottom=0.15, top=0.9)
+    #plt.subplots_adjust(left=0.1, right=0.95, bottom=0.15, top=0.9)
 
     # Create path for pdf export
     pdf_path_j = os.path.join(args['base_dir'], args['pdf_name_j'])
@@ -72,7 +72,7 @@ def main(args):
     print(f"Joined plot printed to {pdf_path_j}")
 
     # Create the annular plot and axis objects
-    fig2, ax_annular = plt.subplots(figsize=(8,4))
+    fig2, ax_annular = plt.subplots(figsize=(8,5), tight_layout=True)
 
     # Call the plot_annular_data function for each CSV file to plot
     for i in range(1, 7):
@@ -81,7 +81,7 @@ def main(args):
             annular_xlabel=args['annular_xlabel'], annular_ylabel=args['annular_ylabel'], annular_title=args['annular_title'])
 
     # Adjust the plot margins to ensure all axes labels are visible
-    plt.subplots_adjust(left=0.1, right=0.95, bottom=0.15, top=0.9)
+    #plt.subplots_adjust(left=0.1, right=0.95, bottom=0.15, top=0.9)
 
     # Create path for pdf export
     pdf_path_a = os.path.join(args['base_dir'], args['pdf_name_a'])
@@ -100,8 +100,8 @@ def plot_joined_data(fig1, ax_joined, file_path, label, color, joined_xlabel, jo
 
     # Extract the relevant columns
     n_exp = data['n_exp']
-    mean_joined_density = data['mean_joined_match']/350
-    std_joined_density = data['std_joined_match']/350
+    mean_joined_density = data['mean_joined_match']/337
+    std_joined_density = data['std_joined_match']/337
     
 
     # Plot mean_joined_match vs n_exp with line connecting points and error bars
@@ -111,7 +111,7 @@ def plot_joined_data(fig1, ax_joined, file_path, label, color, joined_xlabel, jo
     # Set the x and y axis labels and title
     ax_joined.set_xlabel(joined_xlabel)
     ax_joined.set_ylabel(joined_ylabel)
-    ax_joined.set_title(joined_title, fontsize=14)
+    ax_joined.set_title(joined_title, fontsize=16)
     #ax_joined.tick_params(axis='both', which='major', labelsize=12)
     ax_joined.grid(True)
 
@@ -120,7 +120,7 @@ def plot_joined_data(fig1, ax_joined, file_path, label, color, joined_xlabel, jo
     ax_joined.set_xticks(range(0, 37, 3))
 
     # Add a legend to the plot
-    ax_joined.legend(fontsize=12)
+    ax_joined.legend(fontsize=14)
 
     return fig1, ax_joined
 
@@ -133,8 +133,8 @@ def plot_annular_data(fig2, ax_annular, file_path, label, color, annular_xlabel,
 
     # Extract the relevant columns
     n_exp = data['n_exp']
-    mean_annular_density = data['mean_annular_match']/350
-    std_annular_density = data['std_annular_match']/350
+    mean_annular_density = data['mean_annular_match']/337
+    std_annular_density = data['std_annular_match']/337
     
     # Plot mean_annular_match vs n_exp with line connecting points and error bars
     ax_annular.errorbar(n_exp, mean_annular_density, yerr=std_annular_density, fmt='o', capsize=4, markersize=5, color=color, label=label)
@@ -143,15 +143,15 @@ def plot_annular_data(fig2, ax_annular, file_path, label, color, annular_xlabel,
     # Set the x and y axis labels and title
     ax_annular.set_xlabel(annular_xlabel)
     ax_annular.set_ylabel(annular_ylabel)
-    ax_annular.set_title(annular_title, fontsize=14)
+    ax_annular.set_title(annular_title, fontsize=16)
     ax_annular.tick_params(axis='both', which='major')
-    ax_joined.grid(True)
+    ax_annular.grid(True)
     # Set the x-axis limits to 0 and 36, with ticks every 3 units
     ax_annular.set_xlim(0, 37)
     ax_annular.set_xticks(range(0, 37, 3))
 
     # Add a legend to the plot
-    ax_annular.legend(fontsize=12)
+    ax_annular.legend(fontsize=14)
 
     return fig2, ax_annular
 
